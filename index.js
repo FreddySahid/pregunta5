b3a376b6638c457eaece586725d24a14c5b0610c
FreddySahid <freddysahid@outlook.com>
2021-12-13 15:51:24 -0600

    listo

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 
 var lista = document.createElement("li");
+ 
+var listaR = document.getElementById("listaR")
 
 
 cargar.addEventListener("click", function(){
     
     
-    axios.get(" https://mocki.io/v1/d4867d8b-b5d5-4a48-a4ab-79131b5809b8", {
-        pregunta : document.getElementById("pregunta").value,
-        video : TmpPath 
+    axios.get(" https://mocki.io/v1/d4867d8b-b5d5-4a48-a4ab-79131b5809b8").then(function (response) {
+        console.log(response.data);
+        lista = response.data;
+        listaR.appendChild(lista)
 
- 
-        
     })
-    .catch(function (error) {
-        console.log(error);
 
         
-    })
 });
